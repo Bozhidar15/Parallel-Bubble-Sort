59b679f183803dbb90dc549a2008d50ca0670e3c
Bozhidar15 <bojidaruse@gmail.com>
2024-04-16 13:57:56 +0300

    extends bugs

--- BubbleSortParallel/src/Main.java
+++ BubbleSortParallel/src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class Main extends Organize {
+public class Main {
 
 
     public static void main(String[] args) {
+
         Organize.createLocks();
     }
 }
